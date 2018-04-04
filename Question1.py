b5a5c259fb30ca92529919eac1c8a7117d6c4d5a
reddyenjamuri <reddy.enjamuri@yale.edu>
2018-04-03 23:50:53 -0400

    final

---
+++ Question1.py
@@ -0,0 +1,12 @@
+import Classes as Cls
+import SupportSteadyState as SSS
+
+# expected rewards from 1000 games
+
+setOfGames = Cls.SetOfGames(id=1, prob_head=0.5, n_games=1000)
+outcomes = setOfGames.simulation()
+
+setOfGames2 = Cls.SetOfGames(id=2, prob_head=0.45, n_games=1000)
+outcomes2 = setOfGames2.simulation()
+
+SSS.print_comparative_outcomes(outcomes, outcomes2)
